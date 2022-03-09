17a2508c211b2e5bf3b29a1053646ea86296f558
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-03-09 22:33:22 +0300

    Added Resposive

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const openMenu = document.querySelector(".open");
+const closeMenu = document.querySelector(".close");
+const navBar = document.querySelector(".nav");
+
+openMenu.addEventListener("click", () => {
+    navBar.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  navBar.classList.remove ("active");
+});
